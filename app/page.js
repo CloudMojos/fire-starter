69342a3e5e85802f7e9b6924b6a20652920571a0
starfart69 <joshuel.simbulan@gmail.com>
2024-11-07 16:24:43 +0800

    Starting tailwind page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <p className="">Hello world</p>
-  );
+    return (
+        <header>
+            {/* User Profile */}
+            <div className="flex item-center gap-2">
+                {/* User Picture */}
+                <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+                    <img
+                        className="object-cover w-full h-full"
+                        src="https://thispersondoesnotexist.com/image"
+                        alt="Profile Picture"
+                    />
+                </div>
+
+                {/* Greetings */}
+                <small>Hi, Joshuel!</small>
+            </div>
+        </header>
+    );
 }
